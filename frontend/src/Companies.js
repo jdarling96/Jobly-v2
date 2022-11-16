558a3bee36cb9748bf8b0778960680258df05657
Josh Darling <joshua.a.darling@gmail.com>
2022-11-16 12:43:38 -0700

    Companies search filter implemented using api call to backend. Cards for each company displaying. Styling needed.

--- frontend/src/Companies.js
+++ frontend/src/Companies.js
@@ -1,47 +1,76 @@
-import React,{useState, useEffect} from "react";
-import { Spinner } from 'react-spinners-css';
-import JoblyApi from './api'
+import React, { useState, useEffect } from "react";
+import { Spinner } from "react-spinners-css";
+import { Form, FormGroup, Input, Button } from "reactstrap";
+import JoblyApi from "./api";
 import CompanyCard from "./CompanyCard";
 
 const Companies = () => {
-    const initialState = {
-        companies:[]
+  const [companyList, setCompanyList] = useState({ companies: [] });
+  const [searchFilter, setSearchFilter] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // for search filters the values of the 3 params cannot be left blank eg name= ...
+
+  useEffect(() => {
+    async function getCompanyList(searchFilter) {
+      let companies = await JoblyApi.getAllCompanies(searchFilter);
+      setCompanyList(() => ({
+        companies: [...companies],
+      }));
+      setIsLoading(false);
     }
-    const [companyList, setCompanyList] = useState(initialState)
-    const [searchFilter, setSearchFilter] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
+    getCompanyList(searchFilter);
+  }, [searchFilter]);
 
-// for search filters the values of the 3 params cannot be left blank eg name= ...
-
-    useEffect(()=> {
-        async function getCompanyList(searchFilter) {
-            let companies = await JoblyApi.getAllCompanies(searchFilter)
-            setCompanyList(() => ({
-                companies: [...companies]
-            }))
-            //setIsLoading(false)
-        }
-        getCompanyList(searchFilter)
-
-    },[searchFilter])
-
-     console.log(companyList)
-     console.log(companyList.companies.length)
-    
-    if (companyList.companies.length === 0) { 
-    return <div className="loading"><Spinner/></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    if (value === "") {
+      setSearchFilter();
+    } else {
+      setSearchFilter({ name: value });
     }
-    
 
+    console.log(e.target[0].value);
+  };
+
+  if (isLoading) {
     return (
-        <div>
-            {companyList.companies.map(c => (
-                <CompanyCard key={c.handle} name={c.name} description={c.description} numEmployees={c.numEmployees} logo={c.logoUrl}  />
-            ))}
-            
+      <div className="loading">
+        <Spinner />
+      </div>
+    );
+  }
 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Input
+            id="searchFilter"
+            name="search"
+            placeholder="Enter a Search Term..."
+            type="text"
+          />
+          <Button>Search</Button>
+        </FormGroup>
+      </Form>
+      {companyList.companies.length === 0 ? (
+        <p>You Suck!!</p>
+      ) : (
+        companyList.companies.map((c) => (
+          <CompanyCard
+            key={c.handle}
+            handle={c.handle}
+            name={c.name}
+            description={c.description}
+            numEmployees={c.numEmployees}
+            logo={c.logoUrl}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
-export default Companies
+export default Companies;
--- frontend/src/CompanyCard.js
+++ frontend/src/CompanyCard.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Card, CardTitle, CardBody, CardText } from 'reactstrap'
+import { Card, CardTitle, CardBody, CardText, CardImg } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 
 const CompanyCard = ({ name, description, numEmployees, logo }) => {
 
     return (
         <div>
+            <Card className="my-2">
+                <CardBody>
+                    <CardTitle>
+                       {name}
+                    </CardTitle>
+                    <CardText>
+                        {description}
+
+                    </CardText>
+                    <CardText>
+                        <small className="text-muted">
+                            Number of Employees: {numEmployees}
+                        </small>
+                    </CardText>
+                </CardBody>
+                <CardImg
+                alt="card logo of company" 
+                src={logo}/>
+            </Card>
 
         </div>
     )
